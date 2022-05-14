d4d2ef77c41465f51c4f9db8e188bcfbb993ba07
Gaston Bujia <gastonbujia@gmail.com>
2022-05-14 18:16:57 -0300

    added base for run all expirements (config.json and wrapper.sh)

---
+++ notebooks/run_nss.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+# Run the NSS feature calculation for all the videos in the given a configuration file.
+# TODO: This script is not yet finished.
+for i in {1..10}
+    bash python3 /path/to_your/python/script.py
