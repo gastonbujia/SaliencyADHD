fa65cc1b5ec3d8dacfe8b3fa16a1fac42e139cd4
Gaston Bujia <gastonbujia@gmail.com>
2022-05-16 12:37:05 -0300

    checked function and added: create_frame_tables

---
+++ notebooks/create_frame_tables.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import pandas as pd
+from scripts.utils import *
+from scripts.data import *
+
+if __name__=='__main__':
+    VIDEO = 'WK'
+    results_path = '../results/'
+    file = 'results_nss_diary.csv'
+    VIDEO_NAME = 'Diary'
+    NFRAMES = 2817
+
+    # load videos data
+    videos_data = load_video_data()
+
+    # read NSS files
+    df = pd.read_csv(os.path.join(results_path, VIDEO, file))
+    df_means = df.groupby('ID')[['NSS_vn','NSS_fg']].mean()
+
+    # compare both NSS files
+    df_r = pd.read_csv(os.path.join('R', 'FinalTableH3bWK.csv'),sep=';')
+    df_compare = pd.merge(df_r, df_means, on='ID', how='inner')
+    df_compare.head()[['ID','NSS_fg_x', 'NSS_fg_y', 'NSS_vn_x', 'NSS_vn_y']]
+    
+    # filter subjects
+    ids_data = df_r.ID.unique()
+
+    # create timeseries table - align NSS files
+    ts_vn = create_timeseries_matrix(df[df.ID.isin(ids_data)].reset_index(), 
+                                                  metric_val='NSS_vn',
+                                                  nframes = NFRAMES)
+
+    ts_fg = create_timeseries_matrix(df[df.ID.isin(ids_data)].reset_index(), 
+                                                  metric_val='NSS_fg',
+                                                  nframes = NFRAMES)
+
+    # check/create save path
+    results_dir = './results'
+    if not os.path.exists(results_dir): os.mkdir(results_dir)
+    video_results_dir = os.path.join(results_dir, VIDEO)
+    if not os.path.exists(video_results_dir): os.mkdir(video_results_dir)
+    
+    # filter timeseries table (subset of frames)
+    for fr in range(0, NFRAMES, 5):
+        # exclude tables of unneded frames fr.isin(frame_list)
+        if True:
+            # format data for R
+            aux = df_r.drop(columns=['NSS_vn', 'NSS_fg'])
+            #print(aux.shape)
+            vn_col = ts_vn.T.iloc[:,fr].rename('NSS_vn')
+            aux = pd.merge(aux, vn_col, left_on='ID', right_index=True, how='inner')
+            # TODO: perdi unos 60 sujetos seguramente en la funcion de create timeseries matrix
+            #print(aux.shape)
+            fg_col = ts_fg.T.iloc[:,fr].rename('NSS_fg')
+            aux = pd.merge(aux, fg_col, left_on='ID', right_index=True, how='inner')
+            # save data in R format for each frame with separator ';'
+            aux.to_csv(os.path.join(video_results_dir, 'table_{}_{}.csv'.format(VIDEO_NAME, fr)), index=False, sep=';')
